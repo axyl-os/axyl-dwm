8ce86feefaea8d12320408b2406afe57ef41affb
Kungger-git <matthewriel63@gmail.com>
2021-10-12 08:12:17 +0800

    minor change in autostart script

--- dwm-configs/autostart.sh
+++ dwm-configs/autostart.sh
@@ -1,17 +1,17 @@
 #!/bin/env bash
 
+# sets wallpaper using feh
+bash $HOME/.dwm/.fehbg
+
+# kill if already running
+killall -9 picom xfce4-power-manager ksuperkey dunst sxhkd dwmblocks
+
 # sets superkey
 ksuperkey -e 'Super_L=Alt_L|F1' &
 ksuperkey -e 'Super_R=Alt_L|F1' &
 
-# sets wallpaper using feh
-bash $HOME/.dwm/.fehbg
-
 # starts dwmblocks
 dwmblocks &
-
-# kill if already running
-killall -9 picom xfce4-power-manager ksuperkey dunst sxhkd
 
 # start hotkey daemon
 sxhkd &
@@ -21,7 +21,7 @@ dunst \
 -geom "280x50-10+38" -frame_width "1" -font "Source Code Pro Medium 10" \
 -lb "#080F1CFF" -lf "#c7c8c8FF" -lfr "#943934FF" \
 -nb "#080F1CFF" -nf "#c7c8c8FF" -nfr "#943934FF" \
--cb "#2E3440FF" -cf "#BF616AFF" -cfr "#BF616AFF" &
+-cb "#080F1CFF" -cf "#BF616AFF" -cfr "#BF616AFF" &
 
 # start compositor and power manager
 xfce4-power-manager &
