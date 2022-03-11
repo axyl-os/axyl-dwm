77da1c446c542e37b24b16256fd35d70a222342c
Kungger-git <matthewriel63@gmail.com>
2022-03-11 14:12:17 +0800

    fix

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -34,11 +34,8 @@ if [[ ! `pidof xfce-polkit` ]]; then
     /usr/lib/xfce-polkit/xfce-polkit &
 fi
 
-# start udiskie
-udiskie &
-
 # replace neovim colorscheme
-sed -i "s/theme =.*$/theme = \"onedark\",/g" $HOME/.config/nvim/lua/chadrc.lua
+sed -i "s/theme =.*$/theme = \"onedark\",/g" $HOME/.config/nvim/lua/custom/chadrc.lua
 
 # change xfce4-terminal colorscheme
 XFCE_TERM_PATH="$HOME/.config/xfce4/terminal"
